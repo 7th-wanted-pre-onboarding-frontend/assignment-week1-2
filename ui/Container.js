ff9aacc8aa2307c8bcea3e1d86be150c66e62ca7
taesung1993 <cheonyulin@gmail.com>
2022-10-29 17:54:44 +0900

    feat: add 반응형컨테이너 컴포넌트 추가

---
+++ ui/Container.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Container = styled.article`
+  display: flex;
+  flex-direction: column;
+  background-color: #fff;
+  width: 100%;
+  max-width: 1024px;
+  min-height: 100vh;
+  margin: 0 auto;
+`;
+
+export default Container;
