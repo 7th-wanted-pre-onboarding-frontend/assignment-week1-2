9ab7647bf8dbbc665af578aa7588a7336e4cbdb7
kimjuno97 <jh241093@gmail.com>
2022-10-30 11:27:47 +0900

    feat: 이슈리스트 레이아웃 구현

---
+++ src/ui/IsuuseItemContainer.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const IsuuseItemContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 100px;
+  border-bottom: 1px solid black;
+  cursor: pointer;
+`;
+
+export default IsuuseItemContainer;
