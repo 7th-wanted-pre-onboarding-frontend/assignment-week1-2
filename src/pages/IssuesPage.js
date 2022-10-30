434c3be77464d3f50b67a120bc39172d3fe224a2
kimjuno97 <jh241093@gmail.com>
2022-10-30 14:59:54 +0900

    feat: 무한스크롤 기능 구현 중

--- src/pages/IssuesPage.js
+++ src/pages/IssuesPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import Container from '../ui/Container';
 import Header from '../components/Header';
 import client from '../utils/CustomAxios';
@@ -6,22 +6,68 @@ import IssueItem from '../components/IssuseItem';
 
 export default function IssuesPage() {
   const [issueList, setIssueList] = useState([]);
+  const [addListToggle, setAddListToggle] = useState(false);
+
+  const viewport = useRef(null);
+  const target = useRef(null);
+
+  const loadItems = async () => {
+    setAddListToggle((prev) => !prev);
+    await client
+      .get('/repos/angular/angular-cli/issues?sort=comments&page=11&per_page=5')
+      .then(({ data }) => {
+        setIssueList((prev) => [...prev, ...data]);
+        setAddListToggle((prev) => !prev);
+      });
+  };
+
   useEffect(() => {
     const getIssue = async () => {
-      const { data } = await client.get(
-        '/repos/angular/angular-cli/issues?sort=comments&per_page=1'
-      );
-      setIssueList(data);
+      await client
+        .get(
+          '/repos/angular/angular-cli/issues?sort=comments&page=1&per_page=12'
+        )
+        .then(({ data }) => {
+          setIssueList(data);
+          setAddListToggle((prev) => !prev);
+        });
     };
     getIssue();
   }, []);
 
+  useEffect(() => {
+    const options = {
+      root: viewport.current,
+      threshold: 0
+    };
+
+    const handleIntersection = (entries, observer) => {
+      entries.forEach(async (entry) => {
+        if (!entry.isIntersecting) {
+          return;
+        }
+
+        await loadItems();
+        observer.unobserve(entry.target);
+        observer.observe(target.current);
+      });
+    };
+
+    const io = new IntersectionObserver(handleIntersection, options);
+
+    if (target.current) {
+      io.observe(viewport.current);
+    }
+    return () => io && io.disconnect();
+  }, [viewport, target, addListToggle]);
+
   return (
-    <Container>
+    <Container ref={viewport}>
       <Header />
       {issueList?.map((oneIssue) => (
         <IssueItem key={oneIssue.id} oneIssue={oneIssue} />
       ))}
+      <div ref={target}>불러오는 중입니다</div>
     </Container>
   );
 }
