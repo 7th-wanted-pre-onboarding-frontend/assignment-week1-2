4f107c33f5349ac1c44c21d383754b976c44768a
task11 <task11@naver.com>
2022-10-30 23:54:25 +0900

    feat: 404 not found page 컴포넌트 구현

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -1,6 +1,24 @@
 import React from 'react';
+
 import Container from '../ui/Container';
 
+import {
+  StyledComment,
+  StyledCommentIcon,
+  StyledCommentTitle,
+  StyledGoHome,
+  StyledTitle
+} from '../ui/NotFoundPage';
+
 export default function NotFoundPage() {
-  return <Container>NotFoundPage</Container>;
+  return (
+    <Container>
+      <StyledTitle>
+        <StyledCommentIcon>😭</StyledCommentIcon>
+        <StyledCommentTitle>404</StyledCommentTitle>
+        <StyledComment>Not Found</StyledComment>
+        <StyledGoHome to='/'>Go Home</StyledGoHome>
+      </StyledTitle>
+    </Container>
+  );
 }
