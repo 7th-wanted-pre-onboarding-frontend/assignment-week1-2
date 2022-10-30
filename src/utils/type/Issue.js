f7e5bba706d27ccb9866f7428a05f73321d0a957
pmb087 <pmb087@gmail.com>
2022-10-30 13:48:12 +0900

    feat: 이슈타입 속성 추가

--- src/utils/type/Issue.js
+++ src/utils/type/Issue.js
@@ -1,10 +1,12 @@
 class Issue {
-  constructor(user, createdAt, number, title, comments) {
+  constructor(user, createdAt, number, title, comments, userImage, contents) {
     this.user = user;
     this.createdAt = createdAt;
     this.number = number;
     this.title = title;
     this.comments = comments;
+    this.userImage = userImage;
+    this.contents = contents;
   }
 }
 
