a60d9928edb2cf4e108cc1c0ea456629d6607af5
taesung1993 <cheonyulin@gmail.com>
2022-10-31 10:03:31 +0900

    feat: media query 이용한 반응형 디자인 추가

--- src/ui/DetailContent.js
+++ src/ui/DetailContent.js
@@ -1,7 +1,17 @@
 import styled from 'styled-components';
 
-const DetailContent = styled.p`
-  font-size: 1.2rem;
+const DetailContent = styled.div`
+  font-size: 13px;
+  display: flex;
+  align-items: center;
+  gap: 0.8rem;
+  padding: 10px 0 0px 0;
+  color: #999;
+
+  @media (max-width: 400px) {
+    flex-direction: column;
+    align-items: flex-start;
+  }
 `;
 
 export default DetailContent;
